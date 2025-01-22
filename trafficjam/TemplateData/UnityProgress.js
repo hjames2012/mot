c7510534dc1571323f16dec6d855b470028b5e70
hjames2012 <h.c.james2012@outlook.com>
2025-01-22 08:57:35 -0800

    Update UnityProgress.js

--- trafficjam/TemplateData/UnityProgress.js
+++ trafficjam/TemplateData/UnityProgress.js
@@ -29,20 +29,20 @@ function UnityProgress(gameInstance, progress) {
     gameInstance.progress.full.style.width = (100 * progress) + "%";
     gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
 
-    gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' + ' <img src="' + rootPath + '/gears.gif" class="spinner" />';
+    gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' + ' <img src="/gif/gears.gif" class="spinner" />';
 
     if(progress>= 0.9 && progress<1)
     {
-        gameInstance.textProgress.innerHTML = '100% - Running, Wait..' +' <img src="' + rootPath + '/gears.gif" class="spinner" />';
+        gameInstance.textProgress.innerHTML = '100% - Running, Wait..' +' <img src="/gif/gears.gif" class="spinner" />';
         gameInstance.progress.style.display = 'none';
     }
     else
     {
-        gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' + ' <img src="' + rootPath + '/gears.gif" class="spinner" />';
+        gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' + ' <img src="/gif/gears.gif" class="spinner" />';
     }
 
     if (progress == 1) {
-        gameInstance.textProgress.innerHTML = 'Running, Please Wait.. <img src="' + rootPath + '/gears.gif" class="spinner" />';
+        gameInstance.textProgress.innerHTML = 'Running, Please Wait.. <img src="/gif/gears.gif" class="spinner" />';
         gameInstance.progress.style.display = 'none';
     }
 
