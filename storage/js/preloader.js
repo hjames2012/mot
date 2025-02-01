0f5b631a85e2cbabaec3a415e876f3cacadd4866
hjames2012 <h.c.james2012@outlook.com>
2025-01-31 18:03:40 -0800

    big update

---
+++ storage/js/preloader.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('fade-out');
+    setTimeout(() => {
+        preloader.style.display = 'none';
+    }, 500);
+});
