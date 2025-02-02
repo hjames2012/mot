c916fc9bfa26c99ae2756fcc7c26070abb145afa
hjames2012 <h.c.james2012@outlook.com>
2025-02-01 16:34:39 -0800

    big mot

--- storage/js/preloader.js
+++ storage/js/preloader.js
@@ -1,7 +1,12 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', handlePreloader);
+window.addEventListener('load', handlePreloader);
+
+function handlePreloader() {
     const preloader = document.querySelector('.preloader');
-    preloader.classList.add('fade-out');
-    setTimeout(() => {
-        preloader.style.display = 'none';
-    }, 7000);
-});
+    if (preloader) {
+        preloader.classList.add('fade-out');
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 7000);
+    }
+}
