62936e71cba715eeda6952cd1e7dfc30dd377131
hjames2012 <h.c.james2012@outlook.com>
2025-03-13 12:23:44 -0700

    Delete server directory

--- server/uv.config.js
+++
@@ -1,8 +0,0 @@
-self.__uv$config = {
-  prefix: '/proxy/',
-  bare: 'https://ultraviolet-node.tomp.app/', // Default Ultraviolet bare server
-  handler: 'uv.handler.js',
-  bundle: 'uv.bundle.js',
-  config: 'uv.config.js',
-  sw: 'uv.sw.js',
-};
