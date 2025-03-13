3a04a875a4ab7df1db5d779b4997f2ebcb2dca5f
hjames2012 <h.c.james2012@outlook.com>
2025-03-13 09:16:17 -0700

    Update uv.config.js

--- server/uv.config.js
+++ server/uv.config.js
@@ -1,8 +1,6 @@
 self.__uv$config = {
   prefix: '/proxy/',
   bare: 'https://ultraviolet-node.tomp.app/', // Default Ultraviolet bare server
-  encodeUrl: Ultraviolet.codec.base64.encode, // Use base64 encoding for simplicity
-  decodeUrl: Ultraviolet.codec.base64.decode, // Use base64 decoding
   handler: 'uv.handler.js',
   bundle: 'uv.bundle.js',
   config: 'uv.config.js',
