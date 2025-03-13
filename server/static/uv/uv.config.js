98be75e78d10d0089af7d9b56f2633e0716532f2
hjames2012 <h.c.james2012@outlook.com>
2025-03-13 15:02:11 -0700

    Update uv.config.js

--- server/static/uv/uv.config.js
+++ server/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://bare.galacticnetwork.org/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv.handler.js',
