774d24c3bc8d667e8acedc58a6dd8423c7e10949
hjames2012 <h.c.james2012@outlook.com>
2025-03-12 17:38:01 -0700

    Update uv.config.js

--- server/uv.config.js
+++ server/uv.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: 'https://incog.dev/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    prefix: '/',
+  bare: 'https://ultraviolet-node.tomp.app/', // Default Ultraviolet bare server
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv.handler.js',
     bundle: 'uv.bundle.js',
     config: 'uv.config.js',
